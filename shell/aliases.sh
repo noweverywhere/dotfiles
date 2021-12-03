c088312ab7851f107a245d63efa998bbe024be6d
noweverywhere <marinus@noweverywhere.ca>
2021-12-03 09:53:46 -0600

    Update aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -22,4 +22,10 @@ alias current_branch_to_ana="cap staging deploy BRANCH=\$(current_branch) SERVER
 alias current_branch_to_maria="cap staging deploy BRANCH=\$(current_branch) SERVER=maria"
 alias current_branch_to_yara="cap staging deploy BRANCH=\$(current_branch) SERVER=yara"
 alias current_branch_to_eloa="cap staging deploy BRANCH=\$(current_branch) SERVER=eloa"
+alias current_branch_to_luna="cap staging deploy BRANCH=\$(current_branch) SERVER=luna"
+
+alias luna_deploy
+
+alias same_remote="git branch --set-upstream-to=origin/\$(current_branch) \$(current_branch)"
+
 alias erb_lint="bundle exec erblint app/**/*.{html,js,text}{+*,}.erb --autocorrect"
