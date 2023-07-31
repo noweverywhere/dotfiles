f2d391b7e7eca95902de6bc658890b58c690a5a5
noweverywhere <marinus@noweverywhere.ca>
2023-07-31 10:38:04 -0700

    remove deprecated EB aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -18,13 +18,6 @@ alias :e=vim
 alias clean-branches="git branch --merged | egrep -v \"(^\*|master|dev)\" | xargs git branch -d"
 
 alias current_branch="git rev-parse --abbrev-ref HEAD"
-alias current_branch_to_ana="cap staging deploy BRANCH=\$(current_branch) SERVER=ana"
-alias current_branch_to_maria="cap staging deploy BRANCH=\$(current_branch) SERVER=maria"
-alias current_branch_to_yara="cap staging deploy BRANCH=\$(current_branch) SERVER=yara"
-alias current_branch_to_eloa="cap staging deploy BRANCH=\$(current_branch) SERVER=eloa"
-alias current_branch_to_luna="cap staging deploy BRANCH=\$(current_branch) SERVER=luna"
-
-alias luna_deploy
 
 alias same_remote="git branch --set-upstream-to=origin/\$(current_branch) \$(current_branch)"
 
