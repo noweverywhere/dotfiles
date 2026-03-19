b109030057e0cdd78b7b933acb35ca2647b25268
noweverywhere <marinus@noweverywhere.ca>
2026-03-18 17:38:57 -0700

    fix(nvim): align neogit and surround specs
    Place Neogit config in neogit.lua and nvim-surround config in surround.lua.\nKeep plugin file purpose aligned with file naming for easier debugging.

--- nvim/lua/plugins/neogit.lua
+++ nvim/lua/plugins/neogit.lua
@@ -1,10 +1,13 @@
 return {
-    "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
-    config = function()
-        require("nvim-surround").setup({
-            -- Configuration here, or leave empty to use defaults
-        })
-    end
+    "NeogitOrg/neogit",
+    dependencies = {
+        "nvim-lua/plenary.nvim",         -- required
+        "sindrets/diffview.nvim",        -- optional - Diff integration
+
+        -- Only one of these is needed.
+        -- "nvim-telescope/telescope.nvim", -- optional
+        "ibhagwan/fzf-lua",              -- optional
+        -- "echasnovski/mini.pick",         -- optional
+        -- "folke/snacks.nvim",             -- optional
+    },
 }
